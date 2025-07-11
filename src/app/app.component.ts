997ebfada8923805fcb76d1955cda11e394bd0e6
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-07-11 12:07:44 -0300

    feat: add footer component to all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/layout/header/header.component';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { HeaderComponent } from './shared/components/layout/header/header.component'
+import { FooterComponent } from './shared/components/layout/footer/footer.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'mercado-nosso-frontend';
+  title = 'mercado-nosso-frontend'
 }
