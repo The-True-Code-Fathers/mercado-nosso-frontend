efe8f7d499696010506b7ab47e8de5f8d5220af4
Zambrinn <zambrinfilho@gmail.com>
2025-07-12 12:29:58 -0300

    feat: Adicionar rotas para o componente de criação de conta

---
+++ src/app/features/create-account/create-account.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CreateAccountComponent } from './create-account.component';
+
+export const createAccountRoutes: Routes = [
+  {
+    path: '',
+    component: CreateAccountComponent
+  }
+];
